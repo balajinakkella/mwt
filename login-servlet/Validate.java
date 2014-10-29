3d602f87afff5889c2dff9c91a2dab677b5b2344
Balaji Nakkella <kumar.balaji03@gmail.com>
2014-10-29 22:55:33 +0530

    Create Validate.java

---
+++ login-servlet/Validate.java
@@ -0,0 +1,24 @@
+import java.sql.*;
+
+public class Validate
+{
+  public static boolean checkUser(String email,String pass)
+  {
+    boolean st=false;
+    try
+    {
+      Class.forName("com.mysql.jdbc.Driver");
+      PreparedStatement ps=con.prepareStatement("select * from register where email=? and pass=?");
+      ps.setString(1,email);
+      ps.setString(1,pass);
+      ResultSet rs=ps.executeQuery();
+      st=rs.next();
+    }
+    catch(Exception e)
+    {
+      e.printStackTrace();
+    }
+    return st;
+  }
+  
+}
