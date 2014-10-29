aec17bf425d0498b4110b49d345eed409bf3af69
Balaji Nakkella <kumar.balaji03@gmail.com>
2014-10-29 22:58:00 +0530

    Update Validate.java

--- login-servlet/Validate.java
+++ login-servlet/Validate.java
@@ -8,6 +8,7 @@ public class Validate
     try
     {
       Class.forName("com.mysql.jdbc.Driver");
+      Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","","");
       PreparedStatement ps=con.prepareStatement("select * from register where email=? and pass=?");
       ps.setString(1,email);
       ps.setString(1,pass);
